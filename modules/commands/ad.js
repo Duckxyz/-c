3689faf362bae6d3a245c43593a3b29b578328e4
Duckxyz <87573289+Duckxyz@users.noreply.github.com>
2021-08-10 17:21:42 +0700

    Create ad.js

---
+++ modules/commands/ad.js
@@ -0,0 +1,13 @@
+module.exports.config = {
+	name: "ad",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Hà Mạc Trường Giang",
+	description: "Thông tin về admin",
+	commandCategory: "Thông tin về admin",
+	cooldowns: 0
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`\n★Thông Tin Admin Bot Này★\nADMIN NAME : Đức \nBiệt Danh: Duck \nGiới Thiệu: Nhà anh không có gì ngoài nợ ngân hàng 10 tỉ\nI Have Something To You\nI Love You\nTính Cách : Tiếp xúc một thời gian sẽ biết 😎 \nChiều cao : 1m6\nSinh ngày : 03/10/2006\nLiên hệ:0337824707 \nSở Thích: Thích chơi game, lập trinhd ,parkout,du lichlịch 
+43kg\nID FB :100030751998443  \nName ID : Đức \nLink Facebook : https://www.facebook.com/100030751998443\nVài lời tới người dùng BOT: Vui lòng không spam khi sử dụng để tránh die bot. Cảm ơn mọi người đã sử dụng đên con bot của mình.\nLưu ý : Đừng có dại dột mà add 2 bot kẻo bị phát hiện là bạn toang đó <3\nCảnh báo : Vui lòng không dùng bot với mục đích xấu hay cố ý report acc facebook\nChúc bạn sử dụng vui vẻ <3\n
+=== D U C K===`, event.threadID, event.messageID);
